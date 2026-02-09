0a47704524f593e3ce97b2ced97262e8eef097fd
jantonioul5 <juan.ulloa6309@alumnos.udg.mx>
2026-02-09 03:02:32 -0600

    Add files via upload

---
+++ PR1.m
@@ -0,0 +1,15 @@
+Ip=0.2;    % kg*m^2
+Mc=0.2;    % kg
+lp=0.3;    % m
+Mp=0.1;      % kg
+Fc=0.2;         % N
+Beq=0.25;      % Ns/m
+g=9.81;       % m/s^2
+Bp=0.2;    % Nms/rad
+
+
+alpha_0_deg = 1;         
+alpha_0=deg2rad(1);     
+xc_0=0;
+d_alpha_0=0;            
+d_xc_0=0;             
